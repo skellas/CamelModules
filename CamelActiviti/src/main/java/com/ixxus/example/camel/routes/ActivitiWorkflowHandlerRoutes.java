88ca9f0305c288bfccffeb90c828927dbea01105
Seth Kellas <seth.kellas@ixxus.com>
2017-02-10 13:56:25 -0500

    Camel Activiti - Getting the async service call to work properly. Had to send it off in its own thread, mimic'ing what would happen in real life.

--- CamelActiviti/src/main/java/com/ixxus/example/camel/routes/ActivitiWorkflowHandlerRoutes.java
+++ CamelActiviti/src/main/java/com/ixxus/example/camel/routes/ActivitiWorkflowHandlerRoutes.java
@@ -16,9 +16,10 @@ import java.util.concurrent.TimeUnit;
  */
 @Component
 public class ActivitiWorkflowHandlerRoutes extends RouteBuilder {
+    public static final String WORKFLOW_PROCESS_ID = "ActivitiWorkflowSample";
     // Start the workflow
-    public static final String WORKFLOW_START_URI = "direct:WorkflowStart";
-    private static final String ACTIVITI_START_URI = "activiti:ActivitiWorkflowSample";
+    public static final String WORKFLOW_START_URI = "seda:WorkflowStart";
+    private static final String ACTIVITI_START_URI = "activiti:" + WORKFLOW_PROCESS_ID;
 
     // Step 1 of the workflow
     private static final String WORKFLOW_STEP_1_URI = "activiti:ActivitiWorkflowSample:step1";
@@ -26,6 +27,7 @@ public class ActivitiWorkflowHandlerRoutes extends RouteBuilder {
     // Step 2 of the workflow
     private static final String WORKFLOW_STEP_2_URI = "activiti:ActivitiWorkflowSample:step2";
     private static final String ACTIVITI_RECEIVER_TASK = "activiti:ActivitiWorkflowSample:step2Completed";
+    private static final String WORKFLOW_PAUSE_URI = "seda:WorkflowPause";
 
     // Step 3 of the workflow
     private static final String WORKFLOW_STEP_3_URI = "activiti:ActivitiWorkflowSample:step3";
@@ -42,12 +44,19 @@ public class ActivitiWorkflowHandlerRoutes extends RouteBuilder {
                 .log(LoggingLevel.INFO, "Process ID:${property.PROCESS_ID_PROPERTY} - STEP 1");
 
         from(WORKFLOW_STEP_2_URI)
-                .log(LoggingLevel.INFO, "Process ID:${property.PROCESS_ID_PROPERTY} - STEP 2 START")
+                .log(LoggingLevel.INFO, "Process ID:${property.PROCESS_ID_PROPERTY} - STEP 2")
+                .to(WORKFLOW_PAUSE_URI);
+
+        from(WORKFLOW_PAUSE_URI)
+                .log(LoggingLevel.INFO, "Process ID:${property.PROCESS_ID_PROPERTY} - PAUSE START")
                 .process(exchange -> {
                     Objects.nonNull(exchange);
-                    TimeUnit.SECONDS.sleep(5);
+                    for (int i = 0; i < 4; i++) {
+                        System.out.println("Waiting: " + i);
+                        TimeUnit.SECONDS.sleep(1);
+                    }
                 })
-                .log(LoggingLevel.INFO, "Process ID:${property.PROCESS_ID_PROPERTY} - STEP 2 COMPLETE")
+                .log(LoggingLevel.INFO, "Process ID:${property.PROCESS_ID_PROPERTY} - PAUSE COMPLETE")
                 .to(ACTIVITI_RECEIVER_TASK);
 
         from(WORKFLOW_STEP_3_URI)
--- CamelActiviti/src/test/java/com/ixxus/example/RunWorkflowSampleTest.java
+++ CamelActiviti/src/test/java/com/ixxus/example/RunWorkflowSampleTest.java
@@ -3,7 +3,6 @@ package com.ixxus.example;
 import com.ixxus.example.camel.routes.ActivitiWorkflowHandlerRoutes;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
-import org.activiti.engine.task.Task;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
 import org.junit.Test;
@@ -15,17 +14,20 @@ import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
 /**
  * All rights reserved.
  * Copyright (c) Ixxus Ltd. 2016
  * <p/>
+ * This test will show how you can create an Activiti job by using Camel routes.
+ * It will start the process, then pass off the exchange to separate camel routes
+ * both syncronously and asyncronously. It will then wait on that last async route
+ * to complete before finishing, using a <receiveTask/> node in the process definition.
  *
  * @author skellas, Ixxus Ltd.
  */
@@ -41,15 +43,43 @@ public class RunWorkflowSampleTest {
     @Autowired
     protected TaskService taskService;
 
+    private String processId;
+    private String bodyText = "Hello World";
+
     @Test
     public void testCreationOfWorkflowThroughCamel() throws Exception {
-        Object o = runWorkflowProducer.requestBody(new String("Hello World"));
-        assertNotNull(o);
-        Map<String, Object> variables = runtimeService.getVariables(o.toString());
-        assertNotNull(variables);
-        List<Task> taskList = taskService.createTaskQuery().processInstanceId(o.toString()).list();
-        assertNotNull(taskList);
-        TimeUnit.SECONDS.sleep(10);
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        Callable<String> startWorkflowTask = () -> {
+            Object o = runWorkflowProducer.requestBody(bodyText);
+            assertNotNull(o);
+            return o.toString();
+        };
+        Runnable checkWorkflowTask = () -> {
+            assertNotNull("We should have gotten a process id from that Future by now.", processId);
+            assertEquals("And that process should still be running.", 1, runtimeService.createProcessInstanceQuery()
+                                          .processDefinitionKey(ActivitiWorkflowHandlerRoutes.WORKFLOW_PROCESS_ID)
+                                          .processInstanceId(processId).count());
+            Map<String, Object> variables = runtimeService.getVariables(processId);
+            assertNotNull("And that process should have at least one variable attached to it.", variables);
+            assertEquals("A variable that was the Body of the Camel request!", bodyText, variables.get("camelBody"));
+        };
+
+        try {
+            // start the workflow
+            Future<String> future = executor.submit(startWorkflowTask);
+            while (!future.isDone()) {
+                System.out.println("waiting in test");
+                // wait a second
+                TimeUnit.SECONDS.sleep(1);
+            }
+            processId = future.get();
+            // check on the workflow
+            executor.execute(checkWorkflowTask);
+            // the job is actually still waiting, so we'll wait before trying to close out the app
+            TimeUnit.SECONDS.sleep(5);
+        } finally {
+            executor.shutdown();
+        }
     }
 
 
